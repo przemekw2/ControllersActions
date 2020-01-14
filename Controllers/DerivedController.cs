4e061a5cdfd575c263fd07b2ef7e98373242b035
Przemek <przemekw2@gmail.com>
2020-01-14 22:07:52 +0100

    strona 520

---
+++ Controllers/DerivedController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+
+        public ViewResult Index() => View("Result", $"This is a derived controller");
+
+        public ViewResult Headers() => View("DictionaryResult",
+            Request.Headers.ToDictionary(kvp => kvp.Key,
+            kvp => kvp.Value.First()));
+
+    }
+}
---
+++ Controllers/PocoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace ControllersActions.Controllers
+{
+    public class PocoController
+    {
+        //public string Index() => "This is a POCO controller";
+
+        [ControllerContext]
+        public ControllerContext ControllerContext { get; set; }
+
+        public ViewResult Index() => new ViewResult()
+        {
+            ViewName = "Result",
+            ViewData = new ViewDataDictionary(
+                new EmptyModelMetadataProvider(),
+                new ModelStateDictionary())
+            {
+                Model = $"This is a POCO controller"
+            }
+        };
+
+        public ViewResult Headers() =>
+            new ViewResult()
+            {
+                ViewName = "DictionaryResult",
+                ViewData = new ViewDataDictionary(
+            new EmptyModelMetadataProvider(),
+            new ModelStateDictionary())
+                {
+                    Model = ControllerContext.HttpContext.Request.Headers
+                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value.First())
+                }
+            };
+
+
+
+    }
+}
